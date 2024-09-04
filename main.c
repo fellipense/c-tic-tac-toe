eb8fa7ce45d0b81205fab824bb8b2c5762f8998a
King <kingzinhu@hotmail.com>
2024-09-04 15:25:47 -0300

    +printTable()

--- main.c
+++ main.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
-void printTable(char*);
+void printTable(char table[3][3])
+{
+    printf("\n");
+    printf(" %c | %c | %c \n", table[0][0], table[0][1], table[0][2]);
+    printf("---|---|---\n");
+    printf(" %c | %c | %c \n", table[1][0], table[1][1], table[1][2]);
+    printf("---|---|---\n");
+    printf(" %c | %c | %c \n", table[2][0], table[2][1], table[2][2]);
+    printf("\n");
+}
 
 int main()
 {
-    char table[9][2] = {
-        "A1", "B1", "C1",
-        'A2', "B2", "C2",
-        'A3', "B3", "B4"
+    char table[3][3] = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
     };
 
-    while(1)
-    {
-        
-    }
-
     return 0;
-}
-
-void printTable(char * table)
-{
-    for (int i = 0; i < 9; i++)
-    {
-        printf("%s", table[i]);
-    }
 }
